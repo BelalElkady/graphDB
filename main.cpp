8ea0849b769c5ff9cdac04d77dbf79872fa9f603
belalelkady <belal.elkady4@gmail.com>
2026-08-02 02:39:42 +0200

    lesson 01

--- main.cpp
+++ main.cpp
@@ -5,8 +5,29 @@ using namespace std;
 
 int main() {
     string line;
+    auto numberOfEdges = 0;
+     unordered_map<string, int> nodes;
     while (getline(cin, line)) {
         if (line.empty()) continue;
-        cout << "TODO" << "\n";
+        stringstream ss(line);
+        string word;
+        while(ss>>word){
+            if(word=="EDGE"){
+                string from, to;
+                ss>>from>>to;
+                nodes[from]++;
+                ++numberOfEdges;
+            }else if(word=="COUNT"){
+                cout<<numberOfEdges<<"\n";
+            }
+            else if(word=="OUT"){
+                string from;
+                ss>>from;
+                cout<<nodes[from]<<"\n";
+      
+            }
+        
+        }
     }
+    
 }
